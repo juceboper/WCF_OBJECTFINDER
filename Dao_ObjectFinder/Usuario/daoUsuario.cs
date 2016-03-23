f3607ca05a3aef82b6b9d667e4d7443538740fb3
juceboper <juceboper@hotmail.com>
2016-03-22 22:34:37 -0400

    Ajuste 3

---
+++ Dao_ObjectFinder/Usuario/daoUsuario.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dao_ObjectFinder.Datos;
+
+namespace Dao_ObjectFinder.Usuario
+{
+    public class daoUsuario:BaseDatos
+    {
+        public daoUsuario()
+        {
+        }
+
+        public void dao_Crear_Usuario(Entities_ObjectFinder.Usuario.entUsuario Usuario)
+        {
+            try 
+	        {
+                using(DbCommand cmd = dbDatos.GetStoredProcCommand("pkg_insert.sp_insert_usuario",new object[]{ Usuario }))
+	            {
+                    dbDatos.ExecuteNonQuery(cmd);
+	            }
+	        }
+	        catch (Exception)
+	        {	
+		        throw;
+	        }  
+        }
+
+        public void dao_Actualizar_Usuario(Entities_ObjectFinder.Usuario.entUsuario Usuario)
+        {
+            try
+            {
+                using(DbCommand cmd = dbDatos.GetStoredProcCommand("pkg_update.sp_update_usuario", new object[] { Usuario }))
+                {
+                    dbDatos.ExecuteNonQuery(cmd);
+                }
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
