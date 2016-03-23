b9b033a2c3fde2086aca4748bbef9067d307558d
juceboper <juceboper@hotmail.com>
2016-03-22 23:00:38 -0400

    Ajuste Clases

---
+++ Entities_ObjectFinder/Registro/entRegistro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities_ObjectFinder.Registro
+{
+    public class entRegistro
+    {
+        public int idObjeto { get; set; }
+
+        public int idUsuario { get; set; }
+
+        public int idFacultad { get; set; }
+
+        public int idEstado { get; set; }
+
+        public string observacion { get; set; }
+    }
+}
---
+++ Entities_ObjectFinder/Usuario/entUsuario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities_ObjectFinder.Usuario
+{
+    public class entUsuario
+    {
+        public string primerNombre { get; set; }
+
+        public string segundoNombre { get; set; }
+
+        public string primerApellido { get; set; }
+
+        public string segundoApellido { get; set; }
+
+        public int idEstado { get; set; }
+
+        public int telefono { get; set; }
+
+        public int celular { get; set; }
+
+        public string email { get; set; }
+
+        public DateTime fechaNacimiento { get; set; }
+
+        public char genero { get; set; }
+
+        public int idIntegracion { get; set; }
+
+        public string usuario { get; set; }
+
+        public string clave { get; set; }
+
+    }
+}
