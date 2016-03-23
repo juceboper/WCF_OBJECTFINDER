6f073083322c0670dbefea2286f69af2fc22da61
juceboper <juceboper@hotmail.com>
2016-03-22 22:33:46 -0400

    Ajuste

---
+++ Dao_ObjectFinder/Datos/BaseDatos.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dao_ObjectFinder.Datos
+{
+    public class BaseDatos
+    {
+        protected Database dbDatos;
+
+        public Database Instance()
+        {
+            string CadenaConexion = "ConexionObjectFinder";
+            return DatabaseFactory.CreateDatabase(CadenaConexion);
+        }
+
+        public BaseDatos()
+        {
+            dbDatos = Instance();
+        }
+    }
+}
